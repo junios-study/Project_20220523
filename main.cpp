795c0437f77d024880a4bea26baba30125eaa8f9
junios <junios@naver.com>
2022-06-07 16:27:10 +0900

    종료 추가 이벤트 처리 추가

--- main.cpp
+++ main.cpp
@@ -33,13 +33,53 @@ int SDL_main(int argc, char* argv[])
 	SDL_Window* MyWindow = SDL_CreateWindow("Maze", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
-	//그래픽 카드가 할일 등록
-	SDL_SetRenderDrawColor(MyRenderer, 0xff, 0x00, 0x00, 0xff);
-	SDL_RenderClear(MyRenderer);
 
-	//등록 된 일 시작
-	SDL_RenderPresent(MyRenderer);
 
-	while (1);
+	bool bRunning = true;
+	SDL_Event MyEvent;
+	int X = 0;
+	while (bRunning)
+	{
+		SDL_PollEvent(&MyEvent);
+
+		switch(MyEvent.type)
+		{
+		case SDL_QUIT:
+			bRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			SDL_Log("PressKey");
+			break;
+		}
+
+
+		//그래픽 카드가 할일 등록
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0x00, 0x00, 0xff);
+		SDL_RenderClear(MyRenderer);
+
+		// Actor들 그려
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0xff, 0x00, 0xff);
+		if (X > 800)
+		{
+			X = X - 100;
+		}
+		else
+		{
+			X++;
+		}
+		SDL_RenderFillRect(MyRenderer, new SDL_Rect{ X, 0, 100, 100 });
+
+
+		//등록 된 일 시작
+		SDL_RenderPresent(MyRenderer);
+	}
+
+
+	SDL_DestroyRenderer(MyRenderer);
+
+	SDL_DestroyWindow(MyWindow);
+
+	SDL_Quit();
+
 	return 0;
 }
